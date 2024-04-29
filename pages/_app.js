6d9d7b9ba3ed678f45ee9d1d3b06d654e755acb3
iffat2002 <113204563+iffat2002@users.noreply.github.com>
2024-04-30 01:46:15 +0500

    footer changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
   const router = useRouter();
   const isNavBarAvailable = router.pathname !== "/sidenavigation";
+  const isFooterAvailable = router.pathname !== "/sidenavigation";
   return (
     <>
-      {isNavBarAvailable && <Navbar></Navbar>}
+      {isNavBarAvailable && <Navbar />}
       <Component {...pageProps} />
+      {isFooterAvailable && <Footer />}
     </>
   );
 }
